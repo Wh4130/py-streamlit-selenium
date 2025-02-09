7af22063648b36923c31b7f725a6103ed98500b3
Huang Lin Chun <huang0jin@gmail.com>
2025-02-09 12:43:54 +0800

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,39 @@ Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart
 """
 
 
-driver = ChromeDriverManager.get_driver()
+
 
 if st.button("點擊開始爬蟲"):
-    driver.get("https://www.cna.com.tw/list/aall.aspx")  
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
+    from selenium.webdriver.common.by import By
+    import time
+    import re
+    import pandas as pd
+
+    # @st.cache_resource
+    def get_driver():
+        return webdriver.Chrome(
+            service=Service(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=options,
+        )
+
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+
+    try:
+        driver = get_driver()        # *** On streamlit cloud
+        driver.get("https://www.cna.com.tw/list/aall.aspx")   
+    except:
+        driver = webdriver.Chrome()  # *** On local
+
+    
 
 
     # * load all news
