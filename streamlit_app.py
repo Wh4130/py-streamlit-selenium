72b97467369712cea0a1d938bd69a6e3d87b43f5
Huang Lin Chun <huang0jin@gmail.com>
2025-02-09 23:31:04 +0800

    disable dev shm usage

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ if st.button("點擊開始爬蟲"):
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
 
     try:
